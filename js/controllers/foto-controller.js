e4041056577fca5b1ae85068c28aef80fee1ffbd
rubenslopes2019 <49501215+rubenslopes2019@users.noreply.github.com>
2019-04-10 23:46:35 -0300

    Add files via upload

---
+++ js/controllers/foto-controller.js
@@ -0,0 +1,48 @@
+angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams) {
+
+        $scope.foto = {};
+        $scope.mensagem = '';
+
+        if($routeParams.fotoId){
+        	$http.get('/v1/fotos/' + $routeParams.fotoId)
+            .success(function(foto){
+            	$scope.foto = foto;
+            })
+            .error(function(erro){
+            	console.log(erro);
+            	$scope.mensagem = 'Não foi possível obter a foto';
+            });
+        }
+
+
+        $scope.submeter = function() {
+            if ($scope.formulario.$valid) {
+            	if($scope.foto._id){
+            		$http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+            		.success(function(){
+            			$scope.mensagem = 'A foto ' + $scope.foto.titulo + ' foi alterada';
+            		})
+            		.error(function(erro) {
+            			console.log(erro);
+            			$scope.mensagem = 'Não foi possível alterar a foto' + $scope.foto.titulo;
+            		})
+
+            	} else {
+
+
+		            $http.post('v1/fotos', $scope.foto)
+		            .success(function() {
+		            	$scope.foto = {};
+		            	$scope.mensagem = 'Foto incluída com sucesso';
+		            })
+		            .error(function(erro){
+		            	$scope.mensagem = 'Foto não incluída';
+		            	console.log(erro);
+		            });
+		        }
+       		 }
+        };
+
+
+
+    });

adc5c82642a87376c39e547eaec5ccf5125ce8b8
rubenslopes2019 <49501215+rubenslopes2019@users.noreply.github.com>
2019-04-14 13:04:52 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ var app = angular.module('myApp', []);
 app.controller("myCtrl", function ($http, $scope) {
     $http.get('http://viacep.com.br/ws/81350000/json').then(function (response) {
         $scope.resultado = response.data;
+        console.log(response);
     });
 });

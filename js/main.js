13bb7c67f41ddfbe05378bf8712b9ae8eee5e7b9
rubenslopes2019 <49501215+rubenslopes2019@users.noreply.github.com>
2019-04-14 15:14:13 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-angular.module('alurapic', [])
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
 .config(function($routeProvider, $locationProvider) {
 
 	$locationProvider.html5Mode(true);

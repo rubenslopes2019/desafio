32c431d5cb1b53b163b72f480aff447d7aa72c7f
rubenslopes2019 <49501215+rubenslopes2019@users.noreply.github.com>
2019-04-14 20:35:22 -0300

    Update desafio-controller.js

--- js/controllers/desafio-controller.js
+++ js/controllers/desafio-controller.js
@@ -1,9 +1,13 @@
 angular.module('alurapic').controller('DesafioController', function($scope, $http) {
 	
+ const httpOptions = {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json', 'responseType': 'text' })
+    }
+	
 	$scope.lancamentos = []; 
 
 	$http.get("lancamento-conta-legado.json")
-	.success(function(JSON.parse(lancamentos)) {
+	.success(function(lancamentos) {
 		$scope.lancamentos = lancamentos;
 	})
 	.error(function(erro) {

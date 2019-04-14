5dab11be609dc7a6a7cad7f8dde5f262b0e672aa
rubenslopes2019 <49501215+rubenslopes2019@users.noreply.github.com>
2019-04-14 15:22:08 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+angular.module('alurapic', ['ngAnimate', 'ngRoute'])
 .config(function($routeProvider, $locationProvider) {
 
 	$routeProvider.when('', {

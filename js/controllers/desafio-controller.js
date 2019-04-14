7fc0204f8156e75e1bda0f0c2d4de7d1380056bc
rubenslopes2019 <49501215+rubenslopes2019@users.noreply.github.com>
2019-04-14 18:21:13 -0300

    Update desafio-controller.js

--- js/controllers/desafio-controller.js
+++ js/controllers/desafio-controller.js
@@ -2,7 +2,7 @@ angular.module('alurapic').controller('DesafioController', function($scope, $htt
 	
 	$scope.lancamentos = []; 
 
-	$http.get('http://viacep.com.br/ws/81350000/json')
+	$http.get('http://viacep.com.br/ws/81350000/json', $scope.lancamentos)
 	.success(function(retorno) {
 		console.log(retorno);
 		$scope.lancamentos = retorno; 

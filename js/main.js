11e1b7338641620d6590e8dae9573320a4f27a1f
rubenslopes2019 <49501215+rubenslopes2019@users.noreply.github.com>
2019-04-14 17:10:48 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 angular.module('alurapic', ['ngAnimate', 'ngRoute'])
 .config(function($routeProvider, $locationProvider) {
-
-	$routeProvider.when('/', {
 		controller: 'DesafioController',
-	});
 }); 

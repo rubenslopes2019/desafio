378d8787b254e01d0c9a43297a201d9f1c300bc1
rubenslopes2019 <49501215+rubenslopes2019@users.noreply.github.com>
2019-04-14 17:53:37 -0300

    Update desafio-controller.js

--- js/controllers/desafio-controller.js
+++ js/controllers/desafio-controller.js
@@ -2,7 +2,7 @@ angular.module('alurapic').controller('DesafioController', function($scope, $htt
 	
 	$scope.lancamentos = []; 
 
-	$http.get('lancamento-conta-legado.json')
+	$http.get('https://rubenslopes2019.github.io/lancamento-conta-legado.json')
 	.success(function(retorno) {
 		console.log(retorno);
 		$scope.lancamentos = retorno; 

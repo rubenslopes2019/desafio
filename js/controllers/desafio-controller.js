8045183428592485ef4112dfbb5b95dd1ffd6d27
rubenslopes2019 <49501215+rubenslopes2019@users.noreply.github.com>
2019-04-14 18:42:12 -0300

    Update desafio-controller.js

--- js/controllers/desafio-controller.js
+++ js/controllers/desafio-controller.js
@@ -1,9 +1,13 @@
 angular.module('alurapic').controller('DesafioController', function($scope, $http) {
 	
-	$http.get('http://viacep.com.br/ws/81350000/json')
-    .subscribe(res => {
-       this.resultado = res.json()
-       console.log(this.resultado);
-    });
+bairro: string;
+    constructor(http: Http){
 
+        http.get('http://viacep.com.br/ws/81350000/json')
+        .subscribe(res => {
+           this.bairro = res.json().bairro;
+           console.log(this.bairro);
+           // Cidade Industrial
+        });
+    }
 });

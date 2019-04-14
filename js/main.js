c6ab2f5c7a9fe450545c36a5820e1b8b5e01ca3d
rubenslopes2019 <49501215+rubenslopes2019@users.noreply.github.com>
2019-04-14 12:27:48 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var app = angular.module('app', []);
 app.controller("myCtrl", function ($http, $scope) {
     $http.get('https://rubenslopes2019.github.io/lancamento-conta-legado.json').then(function (response) {
         $scope.myData = response.data;
